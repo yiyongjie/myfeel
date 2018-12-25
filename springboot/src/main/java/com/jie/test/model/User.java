cb5c6719f58f73128c8388dc835d5ef220ad0e92
yiyj479 <1074129195@qq.com>
2018-12-25 16:41:25 +0800

    继续

--- springboot/src/main/java/com/jie/test/model/User.java
+++ springboot/src/main/java/com/jie/test/model/User.java
@@ -1,14 +1,22 @@
 package com.jie.test.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@ApiModel
 public class User {
+    @ApiModelProperty(value = "id")
     private Long id;
+    @ApiModelProperty(value = "用户名")
     private String username;
+    @ApiModelProperty(value = "姓名")
     private String name;
+    @ApiModelProperty(value = "年龄")
     private int age;
+    @ApiModelProperty(value = "价格")
     private BigDecimal balance;
 }
