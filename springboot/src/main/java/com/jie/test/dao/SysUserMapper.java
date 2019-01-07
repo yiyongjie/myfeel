3e12e244582884a75c960087ee6020ec764e1065
yiyj479 <1074129195@qq.com>
2019-01-07 10:51:23 +0800

    继续

--- springboot/src/main/java/com/jie/test/dao/SysUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.jie.test.dao;
-
-import com.jie.test.model.SysUser;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysUserMapper {
-    int insertSelective(SysUser record);
-}
