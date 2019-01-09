e4588f47cbb1f6c59a1ffde81aee7ca994625b03
yiyj479 <1074129195@qq.com>
2019-01-09 14:36:01 +0800

    继续

--- springboot/src/main/java/com/jie/test/common/util/FreemarkUtil.java
+++ springboot/src/main/java/com/jie/test/common/util/FreemarkUtil.java
@@ -1,183 +1,76 @@
 package com.jie.test.common.util;
 
+import com.jie.test.common.smallDemo.DataUse;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class FreemarkUtil {
+    private static final String CLASS_PATH = "/src/main/java/com/jie/test/model";
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/yyjtest?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf8";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root123";
-    private static final String SQL = "SELECT * FROM ";// 数据库操作
-
-    static {
+    public static void createData(String tableName){
+        Configuration configuration = new Configuration();
+        Writer out = null;
         try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            log.error("can not load jdbc driver", e);
-        }
-    }
+            // step2 获取模版路径
+            Resource resource = new ClassPathResource("templates");
+            File file = resource.getFile();
+            configuration.setDirectoryForTemplateLoading(file);
+            //获取到这个数据库里所有的表
+            List<String> tableNames=DataUse.getTableNames();
+            boolean isContain=tableNames.contains(tableName);
+            if(isContain){
+                    //准备一下模板参数
+                    Map<String, Object> dataMap = new HashMap<String, Object>();
+                    dataMap.put("classPath", "com.jie.test.model");
+//                    dataMap.put("tableName", s);
 
-    /**
-     * 获取数据库连接
-     *
-     * @return
-     */
-    public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            log.error("get connection failure", e);
-        }
-        return conn;
-    }
-
-    /**
-     * 关闭数据库连接
-     * @param conn
-     */
-    public static void closeConnection(Connection conn) {
-        if(conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                log.error("close connection failure", e);
-            }
-        }
-    }
-
-    /**
-     * 获取数据库下的所有表名
-     */
-    public static List<String> getTableNames() throws SQLException {
-        List<String> tableNames = new ArrayList<>();
-        Connection conn = getConnection();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SHOW TABLES");
-        try {
-            while(rs.next()) {
-                tableNames.add(rs.getString(1));
-            }
-        } catch (SQLException e) {
-            log.error("getTableNames failure", e);
-        } finally {
-            try {
-                rs.close();
-                closeConnection(conn);
-            } catch (SQLException e) {
-                log.error("close ResultSet failure", e);
-            }
-        }
-        return tableNames;
-    }
-
-    /**
-     * 获取表中所有字段名称
-     * @param tableName 表名
-     * @return
-     */
-    public static List<String> getColumnNames(String tableName) {
-        List<String> columnNames = new ArrayList<>();
-        //与数据库的连接
-        Connection conn = getConnection();
-        PreparedStatement pStemt = null;
-        String tableSql = SQL + tableName;
-        try {
-            pStemt = conn.prepareStatement(tableSql);
-            //结果集元数据
-            ResultSetMetaData rsmd = pStemt.getMetaData();
-            //表列数
-            int size = rsmd.getColumnCount();
-            for (int i = 0; i < size; i++) {
-                columnNames.add(rsmd.getColumnName(i + 1));
-            }
-        } catch (SQLException e) {
-            log.error("getColumnNames failure", e);
-        } finally {
-            if (pStemt != null) {
-                try {
-                    pStemt.close();
-                    closeConnection(conn);
-                } catch (SQLException e) {
-                    log.error("getColumnNames close pstem and connection failure", e);
+                    //把下斜杠去掉再把每个斜杠后面的字符大写
+                    String[] splitTableName=tableName.split("_");
+                    StringBuffer className=new StringBuffer();
+                    for(int i=0;i<splitTableName.length;i++){
+                        String splitContent=splitTableName[i].substring(0,1).toUpperCase().concat(splitTableName[i].substring(1));
+                        className.append(splitContent);
+                    }
+                    dataMap.put("className", className.toString());
+                    System.out.println(className);
+                    // step4 加载模版文件
+//                    Template template = configuration.getTemplate("test.ftl");
+//                    // step5 生成数据
+//                    //因拆多模块所以加模块，单模块不需要
+//                    String p="springboot"+CLASS_PATH +"/UserT.java";
+////            String p=System.getProperty("user.dir")+"/springboot"+CLASS_PATH +"/UserT.java";
+//                    File docFile = new File(p);
+//                    out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
+//                    // step6 输出文件
+//                    template.process(dataMap, out);
+//                    System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^UserT.java 文件创建成功 !");
                 }
-            }
-        }
-        return columnNames;
-    }
 
-    /**
-     * 获取表中所有字段类型
-     * @param tableName
-     * @return
-     */
-    public static List<String> getColumnTypes(String tableName) {
-        List<String> columnTypes = new ArrayList<>();
-        //与数据库的连接
-        Connection conn = getConnection();
-        PreparedStatement pStemt = null;
-        String tableSql = SQL + tableName;
-        try {
-            pStemt = conn.prepareStatement(tableSql);
-            //结果集元数据
-            ResultSetMetaData rsmd = pStemt.getMetaData();
-            //表列数
-            int size = rsmd.getColumnCount();
-            for (int i = 0; i < size; i++) {
-                columnTypes.add(rsmd.getColumnTypeName(i + 1));
-            }
-        } catch (SQLException e) {
-            log.error("getColumnTypes failure", e);
-        } finally {
-            if (pStemt != null) {
-                try {
-                    pStemt.close();
-                    closeConnection(conn);
-                } catch (SQLException e) {
-                    log.error("getColumnTypes close pstem and connection failure", e);
-                }
-            }
-        }
-        return columnTypes;
-    }
-
-    /**
-     * 获取表中字段的所有注释
-     * @param tableName
-     * @return
-     */
-    public static List<String> getColumnComments(String tableName) {
-        List<String> columnTypes = new ArrayList<>();
-        //与数据库的连接
-        Connection conn = getConnection();
-        PreparedStatement pStemt = null;
-        String tableSql = SQL + tableName;
-        List<String> columnComments = new ArrayList<>();//列名注释集合
-        ResultSet rs = null;
-        try {
-            pStemt = conn.prepareStatement(tableSql);
-            rs = pStemt.executeQuery("show full columns from " + tableName);
-            while (rs.next()) {
-                columnComments.add(rs.getString("Comment"));
-            }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                    closeConnection(conn);
-                } catch (SQLException e) {
-                    log.error("getColumnComments close ResultSet and connection failure", e);
+            try {
+                if (null != out) {
+                    out.flush();
                 }
+            } catch (Exception e2) {
+                e2.printStackTrace();
             }
         }
-        return columnComments;
+    }
+
+    public static void main(String[] args) {
+        createData("user_test");
     }
 }
--- springboot/src/main/resources/templates/test.ftl
+++ springboot/src/main/resources/templates/test.ftl
@@ -1,38 +1,12 @@
 package ${classPath};
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+@Data
+@ApiModel(value = "${classRemark}")
 public class ${className} {
 
-   private Integer ${Id};
-   private String ${userName};
-   private String ${password};
-
-
-
-    public Integer get${Id}(){
-
-        return ${Id};
-    }
-
-    public void set${Id}(Integer ${Id}){
-
-        this.${Id}=${Id};
-    }
-
-    public String get${userName}(){
-        return ${userName};
-    }
-
-    public void set${userName}(String ${userName}){
-        this.${userName}=${userName};
-
-    }
-
-    public String get${password}(){
-        return ${password};
-    }
-
-    public void set${password}(String ${password}){
-        this.${password}=${password};
-    }
-
+   @ApiModelProperty(value = "${remark}")
+   private Integer ${content};
 }
